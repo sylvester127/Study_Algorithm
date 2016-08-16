63ef0078da9652aca8160eb95f8cb7ab4d6646f1
sylvester127 <sylvester127@naver.com>
2016-08-16 20:47:20 +0900

    dd

--- Project/42.c
+++ Project/42.c
@@ -8,14 +8,42 @@ int main()
 	freopen_s(&f, "input.txt", "r", stdin);
 	freopen_s(&f, "output.txt", "w", stdout);
 
-	int n, i, frequency[6];
+	int n, i, frequency[] = { 0,0,0,0,0,0 };
 	srand((unsigned)time(NULL));
 
 	scanf_s("%d", &n);
 
 	for (i = 0; i < n; i++)
 	{
-		rand() % 6 + 1;
+		switch ((rand() % 6) + 1)
+		{
+		case 1:
+			frequency[0]++;
+			break;
+		case 2:
+			frequency[1]++;
+			break;
+		case 3:
+			frequency[2]++;
+			break;
+		case 4:
+			frequency[3]++;
+			break;
+		case 5:
+			frequency[4]++;
+			break;
+		case 6:
+			frequency[5]++;
+			break;
+		default:
+			break;
+		}
+	}
+
+	for (i = 0; i < 6; i++)
+	{
+		float percentage = frequency[i] * 100 / n;
+		printf("%d %d %.2f\n", i + 1, frequency[i], percentage);
 	}
 	return 0;
 }
